bc1f0de675d8ebb3df2113bb448cf17c0891829d
YaffyKoyo <qyaffy@gmail.com>
2021-08-31 19:35:22 +1200

    commit

--- OpenWeatherMap/FirstCall.py
+++ OpenWeatherMap/FirstCall.py
@@ -9,7 +9,7 @@ token_file_path = path.join(dir_path,'OWtoken.txt')
 tokenfile = open(token_file_path,'r')
 token = tokenfile.read()
 
-r = requests.get(f'https://api.openweathermap.org/data/2.5/onecall?lat=-43.55&lon=172.63&units=metric&appid={token}')
+r = requests.get(f'https://api.openweathermap.org/data/2.5/onecall?lat=-43.55&lon=172.63&exclude=minutely&units=metric&appid={token}')
 
 parsed = json.loads(r.text)
 
